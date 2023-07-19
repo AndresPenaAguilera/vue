a5e73636b523fe81363b225a14c69ce12b8b01fc
Andres Peña <p.andres01@hotmail.com>
2023-07-19 14:34:53 -0500

    composition api, counter app

---
+++ composition-api/src/composables/useCounter.js
@@ -0,0 +1,16 @@
+
+import { ref } from 'vue'
+
+const useCounter =( initialValue = 0)=>{
+
+    const counter = ref( initialValue )
+
+    return {
+        counter,
+
+        increase: ()=> counter.value++,
+        decrease: ()=> counter.value--,
+    }
+}
+
+export default useCounter
--- composition-api/src/views/Counter.vue
+++ composition-api/src/views/Counter.vue
@@ -7,31 +7,21 @@
 
 <script>
 
-import { ref } from 'vue'
+import useCounter from '@/composables/useCounter'
 
 export default {
     name:"Counter",
     props:{ },
     emits:[],
     setup(){
-        const counter = ref(5)
-
-        // const increase =()=>{
-        //     counter.value++
-        // }
-
-        // const decrease =()=>{
-        //     counter.value--
-        // }
-
-        return {
-            counter,
-
-            increase: ()=> counter.value++,
-            decrease: ()=> counter.value--,
-        }
+       const { increase, decrease, counter } = useCounter( 0 )
+        
+       return {
+            increase, decrease, counter
+       }
     }
 }
+
 </script>
 
 <style>
