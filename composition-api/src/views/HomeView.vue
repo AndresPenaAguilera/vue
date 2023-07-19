2c45dfaf36f9689f2713b3cdb30371926055ce5d
Andres Peña <p.andres01@hotmail.com>
2023-07-19 15:23:38 -0500

    Ref Vs Reactive

--- composition-api/src/views/HomeView.vue
+++ composition-api/src/views/HomeView.vue
@@ -1,13 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Reactive Vs Ref</h1>
+    <h2>Ref: {{ refExample }}</h2>
+    <h2>Reactive:{{ reactiveExample }}</h2>
+    <button @click="changeRef">changeRef</button>
+    <button @click="changeReactive"> changeReactive</button>
+
+    <h2>Ref: {{ refExampleBool }}</h2>
+    <h2>Reactive:{{ reactiveExampleBool }}</h2>
+    <button @click="changeStatus">changeStatus</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 
+import { ref, reactive} from 'vue'
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+
+  setup(){
+
+    const refExample = ref({ name:'Example', age:10})
+    const reactiveExample = reactive({ name:'Example reactive', age:10})
+
+    const refExampleBool = ref(false)
+    const reactiveExampleBool = reactive(true)
+
+    const changeStatus =()=>{
+      refExampleBool.value = !refExampleBool.value
+      //reactive only can read objects
+       //reactiveExampleBool = !reactiveExampleBool
+    }
+
+    return {
+      refExample,
+      reactiveExample,
+      refExampleBool,
+      reactiveExampleBool,
+
+      changeRef: ()=>{
+        refExample.value.name ='Example changed'
+      },
+      changeReactive: ()=>{
+        reactiveExample.name ='Example changed too'
+      },
+      changeStatus
+    }
+
+  }
 }
 </script>
